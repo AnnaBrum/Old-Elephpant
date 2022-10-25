305471fd5d04fa371223a9e7c370812a79571732
AnnaBrum <annahelenabrumark@gmail.com>
2022-10-25 19:41:07 +0200

    Added an index, header and footer
    Added the basic files for this project.

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-<?= 'The Elephpent in the Room'; ?>
+<?php
+
+declare(strict_types=1);
+require __DIR__ . '/functions.php'; /*Load file "functions" */
+include __DIR__ . '/header.php';
+
+?>
+
+<main>
+    <p><?= date('l d F') ?></p>
+    <h1>GREAT ADVICE</h1>
+    <div class="advice">
+        <p><?= randomAdvice($advice); ?></p>
+    </div>
+</main>
+<?php include __DIR__ . '/footer.php'; ?>
